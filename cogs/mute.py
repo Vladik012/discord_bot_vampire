fd854286c42ed0d43e53f769da7c169ae6b644db
Vladik012 <71886182+Vladik012@users.noreply.github.com>
2022-06-23 14:20:38 +0200

    Add files via upload

--- cogs/mute.py
+++ cogs/mute.py
@@ -29,8 +29,14 @@ class User(commands.Cog):
 		await sleep(time * 360)
 		await member.remove_roles(mute_role)
 
-	@clear.error
-	async def clear_error( self, ctx, error ):
+	@mute.error
+	async def mute( self, ctx, error ):
+		if isinstance( error, commands.MissingRequiredArgument ):
+			await ctx.send( f'{ctx.author.name} пожалуйста укажите кого кинуть в мут' )
+		if isinstance( error, commands.MissingPermissions ):
+			await ctx.send( f'{ctx.author.name} вы не можете использовать данную команду' )
+	@mute.error
+	async def mute_h( self, ctx, error ):
 		if isinstance( error, commands.MissingRequiredArgument ):
 			await ctx.send( f'{ctx.author.name} пожалуйста укажите кого кинуть в мут' )
 		if isinstance( error, commands.MissingPermissions ):
