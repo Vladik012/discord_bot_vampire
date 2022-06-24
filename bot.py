52bb699e50361e6e09781f94b261a6a6744bc435
Vladik012 <71886182+Vladik012@users.noreply.github.com>
2022-06-24 15:31:01 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,52 +6,54 @@ from discord.utils import get
 import json
 from Cybernator import Paginator as pag
 
-def get_prefix(bot, message):
-	with open('prefixes.json', 'r') as f:
-		prefixes = json.load(f)
+#def get_prefix(bot, message):
+#	with open('prefixes.json', 'r') as f:
+#		prefixes = json.load(f)
 
-	return prefixes[str(message.guild.id)]
+#	return prefixes[str(message.guild.id)]
 
-bot = commands.Bot(command_prefix = get_prefix)
+bot = commands.Bot(command_prefix = !)
 
 
 @bot.event
 async def on_ready():
 	print('Я готов к работе')
+	
 
-@bot.event
-async def on_guild_join(guild):
-	with open('prefixes.json', 'r') as f:
-		prefixes = json.load(f)
+#@bot.event
+#async def on_guild_join(guild):
+#	with open('prefixes.json', 'r') as f:
+#		prefixes = json.load(f)
+#
+#	prefixes[str(guild.id)] = '.'
+#
+#	with open('prefixes.json', 'w') as f:
+#		json.dump(prefixes, f, indent=4)
 
-	prefixes[str(guild.id)] = '.'
-
-	with open('prefixes.json', 'w') as f:
-		json.dump(prefixes, f, indent=4)
-
-@bot.event
-async def on_guild_remove(guild):
-	with open('prefixes.json', 'r') as f:
-		prefixes = json.load(f)
-
-	prefixes.pop[str(guild.id)]
-
-	with open('prefixes.json', 'w') as f:
-		json.dump(prefixes, f, indent=4)
-
-@bot.command()
-async def changeprefix(ctx, prefix):
-	with open('prefixes.json', 'r') as f:
-		prefixes = json.load(f)
-
-	prefixes[str(ctx.guild.id)] = prefix
-
-	with open('prefixes.json', 'w') as f:
-		json.dump(prefixes, f, indent=4)
-
-	await ctx.send(f'Перфикс изменен на {prefix}')
-
-
+#@bot.event
+#async def on_guild_remove(guild):
+#	with open('prefixes.json', 'r') as f:
+#		prefixes = json.load(f)
+#
+#	prefixes.pop[str(guild.id)]
+#
+#	with open('prefixes.json', 'w') as f:
+#		json.dump(prefixes, f, indent=4)
+#
+#@bot.command()
+#async def changeprefix(ctx, prefix):
+#	with open('prefixes.json', 'r') as f:
+#		prefixes = json.load(f)
+#
+#	prefixes[str(ctx.guild.id)] = prefix
+#
+#	with open('prefixes.json', 'w') as f:
+#		json.dump(prefixes, f, indent=4)
+#
+#	await ctx.send(f'Перфикс изменен на {prefix}')
+	
+	
+	
 bot.remove_command('help')
 
 @bot.event
@@ -132,6 +134,7 @@ async def help(ctx):
 		`.шар` - Комманда шар (.шар **Ваш вопрос**)
 		`.invite` - Пригласить бота и зайти на сервер поддержки
 		`.bug` - сообщить о баге
+		
 
 
 		''', color = 0x3498db)
